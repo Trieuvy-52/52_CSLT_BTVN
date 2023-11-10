f931613635e42e8315c44b77525003665d33bb56
Ngo Trieu Vy <trieuvynn@gmail.com>
2023-11-10 23:52:07 +0700

    sua doi lan 2

--- BTN.38.py
+++ BTN.38.py
@@ -3,7 +3,7 @@ if n in ["thang 1","thang 3","thang 5","thang 7","thang 8","thang 10","thang 12"
     print(n,"co 31 ngay")
 elif n in ["thang 4","thang 6","thang 9","thang 11"]:
     print(n,"co 30 ngay")
-elif n=="thang 2":
+elif n=="thang 21":
     print(n, "co 28 hoac 29 ngay")
 else:
     print("thang",n,"khong ton tai")
